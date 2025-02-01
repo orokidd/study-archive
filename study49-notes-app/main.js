bb1799ce9813d70090cac9063a500f9ee2cf5fc5
Arasaka <gregomily@gmail.com>
2025-02-01 10:38:00 +0800

    Ul and small span added via js instead of pre-existing in HTML

--- study49-notes-app/main.js
+++ study49-notes-app/main.js
@@ -6,31 +6,30 @@ const inputButton = document.querySelector("#input-button");
 inputButton.textContent = "Add note";
 
 const outputArea = document.querySelector(".right-area");
-const ulNote = document.querySelector("ul");
+
+const leftArea = document.querySelector(".left-area");
+
+const leftSpan = document.createElement("span");
+leftSpan.id = "small-span";
+leftSpan.textContent = "NOTES";
 
 let data = localStorage.getItem("savedNotes");
 let noteArray = data ? JSON.parse(data) : []; // if data exist, parse/read it, if not make a new array
 
 let activeNote = null;
 
-inputButton.addEventListener("click", () => {
-  if (inputNote.value.trim() === "") {
-    return; // do nothing on empty input
-  } else {
-    addNewNotes();
-    showNotes();
-  }
-});
-
 function showNotes() {
   activeNote = null;
-  ulNote.innerHTML = "";
+  leftArea.innerHTML = "";
+
+  const ulNote = document.createElement("ul");
+
   noteArray.forEach((element, index) => {
     const liList = document.createElement("li");
     const liText = document.createElement("span");
     liText.setAttribute("class", "left-span");
 
-    const formattedText = element.replace(/\n/g, "<br>")
+    const formattedText = element.replace(/\n/g, "<br>");
     liText.innerHTML = formattedText;
 
     const liDelete = document.createElement("button");
@@ -53,6 +52,9 @@ function showNotes() {
       }
     });
 
+    leftArea.appendChild(leftSpan);
+    leftArea.appendChild(ulNote);
+
     ulNote.appendChild(liList);
     liList.appendChild(liText);
     liList.appendChild(liEdit);
@@ -66,8 +68,7 @@ function showNotesMain(index) {
   const notesMain = document.createElement("p");
 
   notesMain.setAttribute("class", "main-para");
-  // const formattedText = noteArray[index].replace(/\n/g, "<br>");
-  const formattedText = noteArray[index]
+  const formattedText = noteArray[index];
   notesMain.innerHTML = formattedText;
 
   outputArea.innerHTML = "";
@@ -96,7 +97,7 @@ function editNotes(index, event) {
 function activeNotes(index) {
   const liList = document.querySelectorAll("li");
   const spanList = document.querySelectorAll(".left-span");
-  const formattedText = noteArray[index]
+  const formattedText = noteArray[index];
 
   for (let list of liList) {
     list.style.removeProperty("border");
@@ -104,17 +105,15 @@ function activeNotes(index) {
     // remove style added in js return to default css
   }
 
-  liList[index].style.border = "solid 1px blue";
+  liList[index].style.border = "solid 1px rgb(148, 148, 148)";
   liList[index].style.backgroundColor = "rgba(24, 24, 24, 0.637)";
   spanList[index].innerHTML = formattedText;
 }
-
 
 function deleteNotes(index, event) {
   event.stopPropagation();
   noteArray.splice(index, 1);
   localStorage.setItem("savedNotes", JSON.stringify(noteArray));
-  ulNote.innerHTML = "";
   outputArea.innerHTML = "";
 
   outputArea.appendChild(inputNote);
@@ -126,27 +125,22 @@ function addNewNotes() {
   noteArray.push(inputNote.value);
   localStorage.setItem("savedNotes", JSON.stringify(noteArray));
   console.log(localStorage.getItem("savedNotes"));
-  ulNote.innerHTML = "";
   inputNote.value = "";
 }
-
-// function selectedNotes(event) {
-//   const liList = document.querySelectorAll("li");
-
-//   for (let list of liList) {
-//     list.style.removeProperty("border");
-//     list.style.removeProperty("background-color");
-//     // remove style added in js return to default css
-//   }
-
-//   event.currentTarget.style.border = "solid 1px blue";
-//   event.currentTarget.style.backgroundColor = "rgba(24, 24, 24, 0.637)";
-// }
 
 function returnDefault() {
   outputArea.innerHTML = "";
   outputArea.appendChild(inputNote);
   outputArea.appendChild(inputButton);
 }
+
+inputButton.addEventListener("click", () => {
+  if (inputNote.value.trim() === "") {
+    return; // do nothing on empty input
+  } else {
+    addNewNotes();
+    showNotes();
+  }
+});
 
 showNotes();
