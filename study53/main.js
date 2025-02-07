beaf4af96e4b7cc31a276f55439ff3f4b70e2b48
tarrss <gregomily@gmail.com>
2025-02-07 09:52:15 +0800

    Calculator

---
+++ study53/main.js
@@ -0,0 +1,45 @@
+const numbersButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator")
+const countButton = document.querySelector(".count");
+let waitingForSecondNumber = false;
+let firstNum = ""
+let secondNum = ""
+let operatorNum = ""
+let output = document.querySelector(".hasil");
+output.textContent = ""
+
+numbersButton.forEach((number) => {
+  number.addEventListener('click', () => {
+    if (!waitingForSecondNumber) {
+      firstNumber(number)
+    } else {
+      secondNumber(number)
+    }
+  })
+})
+
+function firstNumber(number) {
+  output.textContent += number.textContent;
+  firstNum += number.textContent;
+}
+
+function secondNumber(number) {
+  output.textContent += number.textContent;
+  secondNum += number.textContent;
+}
+
+operatorButton.forEach((operator) => {
+  operator.addEventListener('click', ()=> clickOperator(operator))
+})
+
+function clickOperator(operator) {
+  waitingForSecondNumber = true;
+  output.textContent += operator.textContent;
+  operatorNum = operator.textContent;
+}
+
+countButton.addEventListener('click', () => {
+  console.log(firstNum)
+  console.log(secondNum)
+  console.log(operatorNum)
+})
