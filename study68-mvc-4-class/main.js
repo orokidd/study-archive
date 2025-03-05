5ec68f7645a97bdfab9ca79dbf7d90ced61d28ea
deandra <gregomily@gmail.com>
2025-03-05 14:08:18 +0800

    practice mvc with classes

---
+++ study68-mvc-4-class/main.js
@@ -0,0 +1,33 @@
+class Model {
+    constructor() {
+        this.message = "This is a sample message";
+        this.secondMessage = "This is a sample message number two";
+    }
+
+    getMessage() {
+        return this.message;
+    }
+}
+
+// class View {
+//     constructor() {
+
+//     }
+
+//     function() {
+        
+//     }
+// }
+
+// class Model {
+//     constructor() {
+
+//     }
+
+//     function() {
+        
+//     }
+// }
+
+const model = new Model();
+console.log(model);
