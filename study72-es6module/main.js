c82108e67ee6722c08a32915cbe2526447899afe
orokidd <gregomily@gmail.com>
2025-04-15 15:20:48 +0800

    es6 modules study

---
+++ study72-es6module/main.js
@@ -0,0 +1,6 @@
+import U, { printName, printAge } from './user.js'
+
+const newUser = new U("Deandra", 23);
+
+printName(newUser);
+printAge(newUser);
---
+++ study72-es6module/user.js
@@ -0,0 +1,17 @@
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+function printName(userObj) {
+    console.log(`The user's name is ${userObj.name}`)
+}
+
+function printAge(userObj) {
+    console.log(`The user's age is ${userObj.age}`)
+}
+
+export default User
+export { printName, printAge }
