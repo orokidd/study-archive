68cedad9d68e781168694114cc56de2f78b46ab0
orokidd <gregomily@gmail.com>
2025-08-17 12:43:07 +0800

    react props studies

---
+++ study93-reactprop/main3.js
@@ -0,0 +1,19 @@
+// passing props to a child component using destructuring
+function Button({ text, color, fontSize }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px"
+  };
+
+  return <button style={buttonStyle}>{text}</button>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <Button text="Click Me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
+    </div>
+  );
+}
