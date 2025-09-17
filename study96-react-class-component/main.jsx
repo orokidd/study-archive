e52859f3b1d6f85d900b7d6d87b6e0632f015d31
orokidd <gregomily@gmail.com>
2025-09-17 15:42:49 +0800

    react class based component

---
+++ study96-react-class-component/main.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class ClassInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: ['Just some demo tasks', 'As an example'],
+      inputVal: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState((state) => ({
+      ...state,
+      inputVal: e.target.value,
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState((state) => ({
+      todos: state.todos.concat(state.inputVal),
+      inputVal: '',
+    }));
+  }
+
+  handleDelete(todo) {
+    this.setState((state) => ({
+      todos: state.todos.filter(t => t !== todo),
+    }));
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>{this.props.name}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task-entry">Enter a task: </label>
+          <input
+            type="text"
+            name="task-entry"
+            value={this.state.inputVal}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <h4>All the tasks!</h4>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo}>{todo} <button onClick={() => this.handleDelete(todo)}>Delete</button></li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default ClassInput;
